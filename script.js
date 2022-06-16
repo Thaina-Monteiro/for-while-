a9ec0f5d977ff2ff19e2f522a736364d4ce3c22d
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-06-16 20:47:33 -0300

    while-for

---
+++ script.js
@@ -0,0 +1,18 @@
+for (let count = 1; count <= 10; count++) {
+  console.log(count)
+}
+
+let contador = 1;
+while (10 >= contador) {
+  console.log(contador)
+  contador++
+}
+
+for (let count = 0; count <= 100; count += 2) {
+
+  console.log(count)
+}
+
+for (let count = 1; count <= 100; count += 2) {
+  console.log(count)
+}
